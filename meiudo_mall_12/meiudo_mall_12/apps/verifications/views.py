74e3fa9cde4234ffad86d7d96a8e4c12b7de310f
summer <948605548@qq.com>
2019-07-01 10:15:52 +0800

    验证码后端逻辑框架

--- meiudo_mall_12/meiudo_mall_12/apps/verifications/views.py
+++ meiudo_mall_12/meiudo_mall_12/apps/verifications/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.views import APIView
+
+# url('^image_codes/(?P<image_code_id>[\w-]+)/$', views.ImageCodeView.as_view()),
+class ImageCodeView(APIView):
+    """
+    图片验证码
+    """
+    pass
