5e5ee502f49d4b4ba6400fe887aa221fb5dbbec7
summer <948605548@qq.com>
2019-07-11 09:48:13 +0800

    redis数据库配置错误更改

--- meiudo_mall_12/meiudo_mall_12/settings/dev.py
+++ meiudo_mall_12/meiudo_mall_12/settings/dev.py
@@ -107,7 +107,7 @@ CACHES = {
     },
     "session": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://1127.0.0.1:6379/1",
+        "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
