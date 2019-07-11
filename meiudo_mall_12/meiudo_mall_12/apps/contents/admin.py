d13823386c741d4fc8edcd8f68ba8073dc7612d1
summer <948605548@qq.com>
2019-07-11 09:41:49 +0800

    创建超级管理员&在admin站点中注册模型类

--- meiudo_mall_12/meiudo_mall_12/apps/contents/admin.py
+++ meiudo_mall_12/meiudo_mall_12/apps/contents/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+
+from . import models
+
+# Register your models here.
+
+admin.site.register(models.ContentCategory)
+admin.site.register(models.Content)
