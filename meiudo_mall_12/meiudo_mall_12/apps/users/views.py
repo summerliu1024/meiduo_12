90a6ee7991ba5f2a3a6f951aa257b1799897cc53
summer <948605548@qq.com>
2019-07-14 17:12:11 +0800

    修改登录视图与url，调用合并购物车功能

--- meiudo_mall_12/meiudo_mall_12/apps/users/views.py
+++ meiudo_mall_12/meiudo_mall_12/apps/users/views.py
@@ -11,7 +11,9 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
+from rest_framework_jwt.views import ObtainJSONWebToken
 
+from carts.utils import merge_cart_cookie_to_redis
 from goods.models import SKU
 from users import serializers, constants
 from users.models import User
@@ -208,3 +210,22 @@ class UserBrowsingHistoryView(CreateAPIView):
 
         s = SKUSerializer(skus, many=True)
         return Response(s.data)
+
+
+class UserAuthorizeView(ObtainJSONWebToken):
+    """
+    用户认证
+    """
+
+    def post(self, request, *args, **kwargs):
+        # 调用父类的方法，获取drf jwt扩展默认的认证用户处理结果
+        response = super().post(request, *args, **kwargs)
+
+        # 仿照drf jwt扩展对于用户登录的认证方式，判断用户是否认证登录成功
+        # 如果用户登录认证成功，则合并购物车
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data.get('user')
+            response = merge_cart_cookie_to_redis(request, user, response)
+
+        return response
