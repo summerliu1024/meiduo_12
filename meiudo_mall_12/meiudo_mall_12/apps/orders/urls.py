fe5eaa63e61ac06efe90cd5b2309a2d6c5ba2678
summer <948605548@qq.com>
2019-07-16 11:23:43 +0800

    增加订单结算路由

---
+++ meiudo_mall_12/meiudo_mall_12/apps/orders/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+urlpatterns = [
+    url(r'^orders/settlement/$', views.OrderSettlementView.as_view()),
+]
