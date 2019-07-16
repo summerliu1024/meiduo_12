15ae395c5348c4ff8b56ec1d6cdb7f930fe85d91
summer <948605548@qq.com>
2019-07-16 15:56:58 +0800

    保存订单数据视图

--- meiudo_mall_12/meiudo_mall_12/apps/orders/views.py
+++ meiudo_mall_12/meiudo_mall_12/apps/orders/views.py
@@ -4,12 +4,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django_redis import get_redis_connection
+from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from goods.models import SKU
-from orders.serialziers import OrderSettlementSerializer
+from orders.serialziers import OrderSettlementSerializer, SaveOrderSerializer
 
 
 class OrderSettlementView(APIView):
@@ -43,3 +44,11 @@ class OrderSettlementView(APIView):
 
         serializer = OrderSettlementSerializer({'freight': freight, 'skus': skus})
         return Response(serializer.data)
+
+
+class SaveOrderView(CreateAPIView):
+    """
+    保存订单
+    """
+    permission_classes = [IsAuthenticated]
+    serializer_class = SaveOrderSerializer
